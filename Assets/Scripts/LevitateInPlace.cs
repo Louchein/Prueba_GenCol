a0c4e46b3c5700d80113f00a6ff3d7e397bb33c8
LuisCárdenas <luacardenasgo@unal.edu.co>
2025-01-12 18:35:36 -0500

    Basic Ingredients Added

---
+++ Assets/Scripts/LevitateInPlace.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevitateInPlace : MonoBehaviour {
+    private Vector3 initialPosition;
+    private float randomOffset;
+    private float offsetRange = 3f;
+
+    // Start is called before the first frame update
+    void Start() {
+        initialPosition = transform.localPosition;
+        randomOffset = Random.Range(-offsetRange, offsetRange);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transform.localPosition = new Vector3(initialPosition.x,
+                                         initialPosition.y + Mathf.Sin((offsetRange/1) * Time.time + randomOffset)/50, 
+                                         initialPosition.z);
+    }
+}
